c25cba310fc60af46c49173c71c20a6ad9669d29
Mang Jai <117694200+788mangjai@users.noreply.github.com>
2022-11-11 11:00:16 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,6 +14,18 @@ $(document).ready(function () {
 })
 
 
+var swiper = new Swiper(".mySwiper", {
+    zoom: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
 
 var swiper = new Swiper(".mySwiperPopuler", {
     grabCursor: true,
